825223eb48a0e462ae3cc73784849bbc4b61d07c
Laureano Furno <laureanofurno@gmail.com>
2025-08-12 21:00:50 -0300

    .

--- abogados/vite.config.ts
+++ abogados/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://voxaive.github.io/abogados_landing_page",
+  base:"https://voxaive.cloud/abogados_landing_page",
 })
